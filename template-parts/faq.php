e48fa9972b2b896289599593a1727b1526ab483e
Kyryll Prytula <k.prytula@gmail.com>
2021-11-22 10:59:56 +0200

    faq

---
+++ template-parts/faq.php
@@ -0,0 +1,198 @@
+<?php
+/*
+ * Template Name: FAQ
+ *
+ */
+
+get_header();
+?>
+
+<div id="content" class="relative site-content">
+
+<!-- FAQ -->
+
+    <div class="mx-auto pt-36 pb-28 container faq-grid">
+        <h1 class="mt-12">FAQs</h1>
+        <p><strong>On Wombats, Farming, DAOs And Everything In Between...</strong></p>
+        <p class="mt-10">As you can already understand, our Wombat NFTs are quite unique and can help us create a new and impactful community-driven financial system.<br>
+            But we get it, it raises quite a lot of questions, so let’s tackle them one by one:
+        </p>
+
+<!-- Stage 1 -->
+        <div class="mt-20">
+            <h4>Stage 1 (Join the Wombateam) questions</h4>
+            <div class="accordion mt-10">
+                <div class="accordion-item border-t">
+                <button aria-expanded="false"><span class="accordion-title">What is the WombaTeam?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>The WombaTeam are a group of 10,000 unique collectible Wombat NFTs that are hand-drawn by our in-house artist and they are full of shit. 
+Wait a minute! Did we just write shit? Holly crap, we did. 
+Why? because they give a shit! Literally.
+Each Wombat can poop a number of turds according to its rarity, and we have big plans on turning these pooping machines into an impactful, community-driven financial system (see stage 3).</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">How much do they cost?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>Minting a Wombat costs 0.05 ETH (+gas fee) and each one will have a different combination of traits (different backgrounds, facial expressions, clothes etc.) - some rarer than others.</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">Can I pick my Wombat or pay more to get a rare one?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>No, it’s a lucky dip type of thing. We want the sale to be fair and square for all members of the community. </p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">Why Wombats?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>Why not? We couldn’t think of a cuter animal :)</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">Are you really going to donate some of the money?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>Yes! We really love wombats and we want to donate 10% of the sales for Wombat related charities and research (This will be decided by the DAO, see stage 3).</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">OK, sounds cool, where do I start?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>You can join our Discord Channel and keep an eye on our launch date announcement.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<!-- Stage 2 -->
+
+        <div class="mt-20">
+            <h4>Stage 2 (Wpoo tokens generation) questions</h4>
+            <div class="accordion mt-10">
+                <div class="accordion-item border-t">
+                <button aria-expanded="false"><span class="accordion-title">Ok I’m confused, what does poop have to do with wombats and tokens?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>We’re glad you asked. These lovely marsupial creatures poop up to 80 cubes per day (yes - they drop that many cube-shaped turds each day. Magical, isn’t it?). We feel it would be a waste not to use this unique piece of information for something good, so we wrapped their poo into tokens (WPOO). Once we hit 3,000 minted Wombats or reach the “pooping day”, your Wombat will start pooping WPOO tokens and trust us, it’s going to be an absolute shit show.</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">How many WPOO Tokens will my Wombat produce?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>The number of WPoo tokens your wombat will produce daily will depend on its rarity. The rarer your wombat - the more tokens you will get, but it should roughly range between 40 to 80. If we could guess what’s going through your mind while reading it, it’s probably something like “holy shit”!</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">When will this start?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>Once we hit 3,000 minted Wombats or reach the “pooping day”</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">How do I collect my tokens? How do I know I have tokens?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>Well, this is where things get really smelly and you’ll actually have to check this shit out. Note that the Wombats (like most of us) don’t like to be surrounded by too much shit, so the WHO (Wombat Health Organization) guidelines are to collect WPOO at least once per week, as afterwards the Wombats will start feeling a bit overwhelmed by all this shit around them and their pooping capacity will reduce by 10% daily. If you’ll neglect your WPOO collecting duties for the whole 2 weeks - your Wombat will get so stressed - it’ll completely stop pooping. Beyond this point, nature will come to their help as the feces will start degrading daily up to the point where after further 2 weeks - there will be no more WPOO left in the burrow. At this point, the only way to get your wombat to start pooping again - will be to thoroughly clean their burrow (claim/unstake) and hope they won’t hold a grudge against you for too long and will get back to the (stinky) business sooner rather than later.</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">Why are the tokens created on Polygon and not on ETH?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>Because you don’t shit where you eat. At least that’s the informal answer. The formal answer is that we care about the environment and don’t want you to spend too much gas on shit. </p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">What can I do with these tokens? </span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>Good question. You can save, trade or put them in a (compostable) bag and throw them in the bin, BUT you can also use them to participate in the Wombat DAO activity by taking part in farming activities (see stage 3).</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<!-- Stage 3 -->
+
+<div class="mt-20">
+            <h4>Stage 3 (Farm Fertilising) questions</h4>
+            <div class="accordion mt-10">
+                <div class="accordion-item border-t">
+                <button aria-expanded="false"><span class="accordion-title">What is this farming activity?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>In general - farming (or Yield farming) is basically an activity of staking/lending your tokens in order to get more tokens/rewards etc. This is done by the use of blockchain-based smart contracts. <br>
+In our case - staking your WPOO tokens (which basically means locking them in a liquidity pool) will reward you with WDAO tokens, which are the Wombat DAO governing currency. </p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">What is DAO?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>A DAO is a Decentralized Autonomous Organization. It’s a fancy name that means that the organisation created is not ruled by a specific owner but by the entire community as a whole (making it decentralized).</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">What’s the purpose of the WDAO?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>The WDAO will act as a collective, community-based financial institution. Together we can decide what can be done with the money earned. We can invest in other projects, we can develop the Wombat economy even further, we can create a non-for-profit fund and donate some of the earnings, we can support our community member’s projects, we can initiate new projects. In short - we can do whatever we want to and can agree upon.</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">How will the WDAO make these decisions?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>We’ll be using discord for discussing the WIPs (Wombat DAO Improvement Proposals) and snapshot.org for voting on the proposals. Every member of the DAO would be able to formulate a proposal and put it out for the community to discuss and afterwards (potentially after a few rounds of refinements) to submit it for voting. In order to prevent spamming of the voting mechanism - a certain voting power threshold will need to be held by the user in order to submit it for voting. In order to pass, a WIP will need to both achieve a quorum and a majority voting in favour of it. You can read our Whitepaper for more in-depth details.</p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">Where does the WDAO get its earnings from?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>This is an excellent question! Unlike most of the DAOs out there, Wombat DAO will be launching without a predefined goal in mind and the DAO members will be the ones raising proposals as to particular projects & directions they’d like to build/run. It’s basically starting with a clean slate and anything should be possible, as the only limiting factor would be your imagination. Some activities could be not for profit, other - for profit and if built and executed properly (this is where the community plays the most important role), these would generate various revenue streams for Wombat DAO treasury.
+For example - half of the royalties from the secondary sales of the WombaTeam NFT’s will go into the treasury. </p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">How can you join the DAO?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>WombaTeam NFT is your entry ticket to the DAO. If you participate in the farming process and stake the shit out of WPOO - you will also receive the actual Wombat DAO governance token, called WDAO. Your WDAO holdings will represent your voting power in the DAO.  </p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">How many WDAO will I get for staking my WPOOs?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>There will be a total of 21M WDAO tokens and these would be distributed over the course of 15 years (this is the average lifespan of a wombat in the wild). The individual rewards will depend on the total number of participants in the staking, amount of WPOO staked and some other parameters, so it’s literally impossible to know in advance. </p>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                <button aria-expanded="false"><span class="accordion-title">Is the team minting any NFT’s for themselves?</span><span class="icon" aria-hidden="true"></span></button>
+                    <div class="accordion-content">
+                        <p>Yes, we will mint 300 Wombats (out of the 10,000) in advance, in order to reserve some for community giveaways, partners and the team.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+<!-- C2A Discord -->
+
+    <div class="w-full text-center bg-[#5F4F44] py-20">
+        <h2>Got More Questions?</h2>
+        <p class="mt-6">Ping us on our Discord Channel<br>
+        and ask away</p>
+        <a href="#discord" class="inline-flex justify-center border-0 mt-12 hover:opacity-80"><svg width="156" height="157" viewBox="0 0 156 157" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M57.8853 108.827C57.8853 108.827 60.5442 105.54 62.7108 102.801C53.5521 100.013 50.1053 94.1862 50.1053 94.1862C50.1053 94.1862 50.8439 94.6842 52.1241 95.4312C52.1734 95.481 52.2719 95.5806 52.4196 95.6304C52.6165 95.7797 52.8627 95.8295 53.0597 95.9789C54.8816 96.9749 56.6542 97.8214 58.3284 98.4688C61.2828 99.6142 64.8281 100.809 68.9151 101.606C74.3315 102.602 80.6343 103 87.5772 101.656C90.9748 101.058 94.4216 100.062 98.0161 98.5186C100.527 97.5725 103.334 96.1781 106.288 94.236C106.288 94.236 102.694 100.212 93.2398 102.901C95.4064 105.689 98.0161 108.777 98.0161 108.777C113.921 108.279 120.076 97.7219 120.076 97.7219C120.076 74.2672 109.686 55.2445 109.686 55.2445C99.2964 47.4263 89.4483 47.6255 89.4483 47.6255L88.4635 48.7708C100.724 52.5554 106.387 58.0332 106.387 58.0332C98.9025 53.9 91.5656 51.8583 84.7212 51.0615C79.5018 50.4639 74.5285 50.6133 70.1461 51.2109C69.7029 51.2109 69.3583 51.2607 68.9151 51.3603C66.4038 51.5595 60.2488 52.5056 52.5673 55.9417C49.9083 57.1866 48.3326 58.0332 48.3326 58.0332C48.3326 58.0332 54.2907 52.3064 67.2409 48.5218L66.5516 47.6255C66.5516 47.6255 56.7035 47.4263 46.3138 55.2943C46.3138 55.2943 35.9241 74.317 35.9241 97.7716C35.9241 97.7219 41.9806 108.329 57.8853 108.827ZM90.827 74.815C94.914 74.815 98.1638 78.4502 98.1638 82.8822C98.1638 87.3142 94.914 90.9494 90.827 90.9494C86.7893 90.9494 83.4902 87.3142 83.4902 82.8822C83.441 78.4502 86.6908 74.815 90.827 74.815ZM64.5327 74.815C68.6196 74.815 71.968 78.4502 71.8695 82.8822C71.8695 87.3142 68.6196 90.9494 64.5327 90.9494C60.495 90.9494 57.1959 87.3142 57.1959 82.8822C57.1959 78.4502 60.3965 74.815 64.5327 74.815Z" fill="#DBC7B4"/>
+<circle cx="78" cy="78.2258" r="75.3723" stroke="#DBC7B4" stroke-width="5"/>
+</svg></a>
+    </div>
+
+</div>
+<script>
+    const items = document.querySelectorAll(".accordion button");
+    function toggleAccordion() {
+        const itemToggle = this.getAttribute('aria-expanded');
+        if (itemToggle == 'false') {
+            this.setAttribute('aria-expanded', 'true');
+        } else {
+            this.setAttribute('aria-expanded', 'false');
+        }
+    }
+    items.forEach(item => item.addEventListener('click', toggleAccordion));
+</script>
+
+<?php
+get_footer(); 
