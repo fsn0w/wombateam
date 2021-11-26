850ae8d815f7a6bec1d25d7d8d2a1361bee84b9c
Kyryll Prytula <k.prytula@gmail.com>
2021-11-26 11:48:16 +0200

    price

--- template-parts/mint.php
+++ template-parts/mint.php
@@ -22,7 +22,7 @@ get_header();
             </div>
             <span class="text-[#37271B] text-lg lg:text-2xl">Price: 
                 <input value="0.05" class="mint-price bg-transparent border-0 inline-block w-12 font-bold text-center text-lg lg:w-14 lg:text-2xl" disabled /> 
-                <span class="text-xs font-bold lg:text-sm">ETH</span></span>
+                <span class="text-xs font-bold lg:text-sm">ETH</span></span> 
             <div class="text-center"><button id="mintbtn" class="btn2 text-xl mt-2 py-4 px-10 lg:py-8 lg:px-16 lg:text-5xl">Mint</button></div>
         </div>
 
