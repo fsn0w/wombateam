32183ac84fa2089d18db0ee674b937a07eeb829d
Kyryll Prytula <k.prytula@gmail.com>
2021-12-12 23:28:46 +0200

    json

--- template-parts/api.php
+++ template-parts/api.php
@@ -4,6 +4,14 @@
  *
  */
 
+function changeHeaders($headers)
+{
+    $headers['Content-Type'] = 'application/json; charset=utf-8';
+
+    return $headers;     
+}
+add_filter('wp_headers', 'changeHeaders');
+
 $path = "/var/www/html/";
 
 $after_reveal = false;
