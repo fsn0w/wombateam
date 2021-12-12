fc51713c12065dfb3a25e4c56c5ce3ba9fb918d0
Kyryll Prytula <k.prytula@gmail.com>
2021-12-12 02:20:43 +0200

    Mainnet

--- template-parts/api.php
+++ template-parts/api.php
@@ -8,12 +8,12 @@ $path = "/var/www/html/";
 
 $after_reveal = false;
 
-$contract="0xb531b3DaA87F3864f45866B5236114b76Af0ECA0";
+$contract="0xb82A72453403f350E14B8bF7dcc6FBD045Cc0d3C";
 $api="PVP6WAPH57Z2TCHRTA5PCTX9H5Q866IC75";
 
 $token = get_query_var('token');
 
-$result = file_get_contents("https://api-rinkeby.etherscan.io/api?module=stats&action=tokensupply&contractaddress=".$contract."&apikey=".$api);
+$result = file_get_contents("https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=".$contract."&apikey=".$api);
 $contract_supply = json_decode($result, true);
 
 if ($after_reveal && $token <= $contract_supply["result"]):
